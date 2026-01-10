af10185a93f8ff6d81d1c34cf3414e6860acf325
Shubham Mahamuni <mahamunishubham22@gmail.com>
2026-01-11 02:22:45 +0530

    added route file

---
+++ app/api/products/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products", {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      return NextResponse.json([], { status: 200 });
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.json([], { status: 200 });
+  }
+}
--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,8 @@
 import { Product } from "../types/product";
 
-const API_URL = "https://fakestoreapi.com/products";
+// const API_URL = "https://fakestoreapi.com/products";
+const API_URL = "/api/products";
+
 
 export async function fetchProducts(): Promise<Product[]> {
   try {
